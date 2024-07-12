6a867ff3eb4a46249e604573ec525e3eb07ab7b0
suv-900 <suvhampaul36@gmail.com>
2024-07-12 18:03:37 +0530

    middleware chain

--- controllers/usercontroller.go
+++ controllers/usercontroller.go
@@ -1,6 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
 
-func CreateUser(w http.Response, r http.Request) {
+	"github.com/gin-gonic/gin"
+	"github.com/suv-900/kl/dao"
+	"github.com/suv-900/kl/models"
+)
+
+func ServerStatus(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func AddUser(c *gin.Context) {
+	var user models.User
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnprocessableEntity)
+		return
+	}
+	err = user.Validate()
+	if err != nil {
+		// //aborts and stores the error
+		// c.AbortWithError(http.StatusBadRequest,err)
+		//why even care about such errors
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	err = dao.AddUser(user)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
 }
