4832cace99e3db26bef92fdb40cea6a418330c19
suv-900 <suvhampaul36@gmail.com>
2024-07-13 10:42:18 +0530

    need a logger

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/suv-900/kl/config"
 	"github.com/suv-900/kl/router"
 )
 
 func main() {
+	c := config.Config{}
+	if err := c.LoadEnv(); err != nil {
+		log.Fatal()
+		return
+	}
+
 	engine := gin.New()
 	router.SetupRouter(engine)
-
 	engine.Run()
 }
