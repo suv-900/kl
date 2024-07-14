5c615937bdfe9d4160b2fb37e4eb59e2ec9f922d
suv-900 <suvhampaul36@gmail.com>
2024-07-14 13:34:37 +0530

    more less code

--- main.go
+++ main.go
@@ -3,20 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/suv-900/kl/common"
 	"github.com/suv-900/kl/dao"
 	"github.com/suv-900/kl/logging"
+	"github.com/suv-900/kl/router"
 )
 
 var log = logging.GetLogger()
 
 func main() {
 
-	if err := common.LoadEnv(); err != nil {
-		log.Critical(err)
-		return
-	}
-	log.Info("Created Config.")
 	if err := dao.Init(); err != nil {
 		log.Error(err)
 		return
@@ -24,6 +19,8 @@ func main() {
 	log.Info("DB connection successfull")
 
 	log.Info("Chores complete")
+	log.Info("Creating Gin Engine")
 	engine := gin.New()
+	router.SetupRouter(engine)
 	engine.Run()
 }
