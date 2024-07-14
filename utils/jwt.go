de1cad8d25e97a1955eedddb998451d9a48f6943
suv-900 <suvhampaul36@gmail.com>
2024-07-14 13:34:57 +0530

    jwt done.

--- utils/jwt.go
+++ utils/jwt.go
@@ -5,10 +5,13 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"github.com/suv-900/kl/common"
+	"github.com/suv-900/kl/logging"
 )
 
 var JWTKEY = common.Config.JWTkey
 var tokenExpiryTime = time.Now().Add(60 * time.Minute)
+
+var log = logging.GetLogger()
 
 type CustomPayload struct {
 	ID uint `json:"id"`
@@ -25,4 +28,22 @@ func GenerateToken(userid uint) (string, error) {
 	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 	token, err := rawToken.SignedString(JWTKEY)
 	return token, err
+}
+
+// i dont even know what i want
+// tokenExpired id tokenInvalid
+func ValidateToken(token string) (bool, uint, bool) {
+	var id uint
+	t, err := jwt.ParseWithClaims(token, &CustomPayload{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWTKEY), nil
+	})
+	if err != nil {
+		log.Error(err)
+		return true, id, false
+	}
+	if p, ok := t.Claims.(*CustomPayload); ok && t.Valid {
+		id = p.ID
+		return false, id, false
+	}
+	return false, id, true
 }
