2fd07319b32cd582bdc937e40de87e884f0ee8a0
suv-900 <suvhampaul36@gmail.com>
2024-07-15 14:31:47 +0530

    new schema

--- models/userprofile.go
+++ models/userprofile.go
@@ -3,15 +3,24 @@ package models
 import (
 	"time"
 
-	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type UserProfile struct {
 	gorm.Model
-	UserID     uint
-	User       User
-	Bio        *string
-	ProfilePic *pq.ByteaArray
-	BirthDate  *time.Time
+	Bio       *string
+	BirthDate *time.Time
+
+	UserID uint
+	User   User `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+type Image struct {
+	gorm.Model
+	Binary []byte
+	Name   string
+	Size   int64
+
+	UserID uint
+	User   User `gorm:"constraint:OnDelete:CASCADE;"`
 }
