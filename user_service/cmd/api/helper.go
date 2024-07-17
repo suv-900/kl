adab1097b85cf4fa2ab36f5ade772e23ac94554b
suv-900 <suvhampaul36@gmail.com>
2024-07-18 01:23:51 +0530

    composite

--- user_service/cmd/api/helper.go
+++ user_service/cmd/api/helper.go
@@ -1,1 +1,22 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type envelope map[string]interface{}
+
+func (app *application) writeJSON(w http.ResponseWriter, data envelope, statusCode int) error {
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+
+	w.WriteHeader(statusCode)
+	w.Write(js)
+
+	return nil
+}
