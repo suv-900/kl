79ff586fb4127d8329d366b2b30963d18067e263
suv-900 <suvhampaul36@gmail.com>
2024-07-18 01:23:28 +0530

    composite

--- user_service/cmd/api/error.go
+++ user_service/cmd/api/error.go
@@ -1,1 +1,15 @@
 package api
+
+import "net/http"
+
+func (app *application) sendResponse(w http.ResponseWriter, statusCode int, message interface{}) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, env, statusCode)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *application) invalidAuthenticationHeader(w http.ResponseWriter, r *http.Request) {
+}
