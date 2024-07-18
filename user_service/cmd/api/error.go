8753ee721bcba242e7fc9ce92a8eac9a978234f5
suv-900 <suvhampaul36@gmail.com>
2024-07-18 23:14:07 +0530

    new errors

--- user_service/cmd/api/error.go
+++ user_service/cmd/api/error.go
@@ -2,14 +2,23 @@ package api
 
 import "net/http"
 
-func (app *application) sendResponse(w http.ResponseWriter, statusCode int, message interface{}) {
+func (app *application) sendErrorResponse(w http.ResponseWriter, statusCode int, message interface{}) {
 	env := envelope{"error": message}
 
 	err := app.writeJSON(w, env, statusCode)
 	if err != nil {
+		//err while sending
+		log.Error(err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
-func (app *application) invalidAuthenticationHeader(w http.ResponseWriter, r *http.Request) {
+func (app *application) invalidAuthenticationHeader(w http.ResponseWriter) {
+	message := "invalid authentication headers."
+	app.sendErrorResponse(w, http.StatusExpectationFailed, message)
+}
+
+func (app *application) invalidToken(w http.ResponseWriter) {
+	message := "token invalid"
+	app.sendErrorResponse(w, http.StatusUnauthorized, message)
 }
