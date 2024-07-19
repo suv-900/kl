05f34c10902d4ed20b4bc7bdbc263578d0ef15a5
suv-900 <suvhampaul36@gmail.com>
2024-07-20 01:31:23 +0530

    utils

---
+++ user_service/cmd/api/utils.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// var JWTKEY = common.Config.JWTkey
+var jwtKey = "JWTKEY"
+var tokenExpiryTime = time.Now().Add(60 * time.Minute)
+var ErrTokenInvalid = errors.New("token invalid")
+
+type CustomPayload struct {
+	ID uint `json:"id"`
+	jwt.StandardClaims
+}
+
+func (app *application) generateToken(userid uint) (string, error) {
+	payload := CustomPayload{
+		ID: userid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: tokenExpiryTime.Unix(),
+		},
+	}
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	token, err := rawToken.SignedString(jwtKey)
+	return token, err
+}
+
+func (app *application) verifyToken(token string) (uint, error) {
+	var userid uint
+	t, err := jwt.ParseWithClaims(token, &CustomPayload{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtKey), nil
+	})
+	if err != nil {
+		//this probably means token is expired
+		return userid, err
+	}
+	if p, ok := t.Claims.(*CustomPayload); ok && t.Valid {
+		//this probably means token is invalid
+		userid = p.ID
+		return userid, nil
+	}
+	return userid, ErrTokenInvalid
+}
+func (app *application) generateHashedPassword(password []byte) (string, error) {
+	var hashedpass string
+	hashedbytes, err := bcrypt.GenerateFromPassword(password, 3)
+	if err != nil {
+		return hashedpass, err
+	}
+	return string(hashedbytes), nil
+}
+
+// nil on success and err on fail
+func (app *application) comparePassword(password []byte, dbPassword []byte) error {
+	return bcrypt.CompareHashAndPassword(dbPassword, password)
+}
