8bbb97f23ceb8f457fb0f7928c493e78b10784bb
suv-900 <suvhampaul36@gmail.com>
2024-07-20 01:33:00 +0530

    changes

--- user_service/cmd/api/main.go
+++ user_service/cmd/api/main.go
@@ -1,22 +1,30 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/suv-900/kl/user_service/logging"
+	"github.com/suv-900/kl/user_service/internal/data"
 )
 
-var log = logging.GetLogger()
+type application struct {
+	models data.Models
+}
 
 func main() {
-	if err := dao.Init(); err != nil {
-		log.Error(err)
+
+	if err := LoadEnvVars(); err != nil {
+		log.Critical("couldnt read env vars: ", err)
+		return
+	}
+
+	if err := DBInit(); err != nil {
+		log.Error("couldnt initialise database: ", err)
 		return
 	}
 	log.Info("DB connection successfull")
 
 	log.Info("Chores complete")
 	log.Info("Creating Gin Engine")
+
 	engine := gin.New()
-	router.SetupRouter(engine)
+	SetupRouter(engine)
 	engine.Run()
 }
