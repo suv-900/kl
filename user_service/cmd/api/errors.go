4a7d3b9ded4fbafbe13614f414924a0e6c2a03ac
suv-900 <suvhampaul36@gmail.com>
2024-07-19 21:57:42 +0530

    change function signature to match with HandlerFunc adapter

---
+++ user_service/cmd/api/errors.go
@@ -0,0 +1,44 @@
+package api
+
+import "net/http"
+
+func (app *application) sendErrorResponse(w http.ResponseWriter, statusCode int, message interface{}) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, env, statusCode)
+	if err != nil {
+		//err while sending
+		log.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *application) invalidAuthenticationHeader(w http.ResponseWriter, r *http.Request) {
+	message := "invalid authentication headers."
+	app.sendErrorResponse(w, http.StatusExpectationFailed, message)
+}
+
+func (app *application) invalidToken(w http.ResponseWriter, r *http.Request) {
+	message := "token invalid"
+	app.sendErrorResponse(w, http.StatusUnauthorized, message)
+}
+
+func (app *application) invalidTokenDeletedUser(w http.ResponseWriter, r *http.Request) {
+	message := "user is deleted[dead token] please create new account."
+	app.sendErrorResponse(w, http.StatusNotFound, message)
+}
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request) {
+	message := "server error"
+	app.sendErrorResponse(w, http.StatusInternalServerError, message)
+}
+
+func (app *application) authenticationRequired(w http.ResponseWriter, r *http.Request) {
+	message := "authentication required."
+	app.sendErrorResponse(w, http.StatusUnauthorized, message)
+}
+
+func (app *application) routeNotFound(w http.ResponseWriter, r *http.Request) {
+	message := "route not available."
+	app.sendErrorResponse(w, http.StatusNotFound, message)
+}
